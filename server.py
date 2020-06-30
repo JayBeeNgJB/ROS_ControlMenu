b7e8b8c3344478d0ffe5c6e28bc6820e85193a5c
Nay Oo Kyaw <ds.nayookyaw@gmail.com>
2020-06-30 19:28:36 +0800

    change python to node js server

--- server.py
+++ server.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify, request
 
 import os
+import json
 
 app = Flask(__name__)
 
 app._static_folder = os.path.abspath("templates/static/")
 
 @app.route('/')
-def auto():
-    return render_template("views/auto.html", message="Manual")
+def index():
+    return render_template("views/index.html", message="Manual")
+@app.route('/get/time')
+def get_times():
+    with open("templates/static/data.json") as data_file:
+        data = json.load(data_file)
+        print (data['total_seconds'])
+        return jsonify({"result" : data['total_seconds']})
 
-@app.route('/auto')
-def manual():
-    return render_template("views/manual.html", message="Auto")
+@app.route('/save/time')
+def save_times():
+    total_seconds = request.args.get('total_seconds', 0, type=int)
+    dictionary =  {
+        "total_seconds" : total_seconds
+    }
+    with open("templates/static/data.json", "w") as outfile:
+        json.dump(dictionary, outfile)
+
+    return jsonify({"result": "success"})
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0', port=7000, debug=True)
---
+++ templates/static/js/index.js
@@ -0,0 +1,188 @@
+// very first load
+setup();
+
+var total_seconds = 0;
+var STOP_TIMER = false;
+
+function setup() {
+    console.log("Init");
+    // $.getJSON($SCRIPT_ROOT + '/get/time', {
+        
+    // }, function(data) {
+    //     total_seconds = data.result;
+    //     setTotalTime();
+    // });
+    auto_manual(true, false);
+}
+
+window.addEventListener('load', (e) => {
+    menu_switch();
+    powerOn();
+    powerOff();
+});
+
+function powerOn() {
+    $(".on_btn").on('click', function() {
+        console.log("Power ON");
+
+        //get timer period
+        $duration = $("#timer_min").val() * 60;
+
+        startTimer($duration);
+
+        //disable power on
+        $(".on_btn").css('pointer-events', 'none');
+        $(".on_btn i").css('color', '#767676');
+        //enable power off
+        $(".off_btn").css('pointer-events', 'auto');
+        $(".off_btn i").css('color', 'red');
+    });
+}
+
+function powerOff() {
+    $(".off_btn").on('click', function() {
+        console.log("Power Off");
+
+        STOP_TIMER = true;
+        //enable power on
+        $(".on_btn").css('pointer-events', 'auto');
+        $(".on_btn i").css('color', 'blue');
+        //disable power off
+        $(".off_btn").css('pointer-events', 'none');
+        $(".off_btn i").css('color', '#767676');
+    });
+}
+
+function startTimer(duration) {
+    var timer = duration, minutes, seconds;
+
+    var countdown = setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        $(".timer").empty().text(minutes + ":" + seconds);
+
+        //show total usage hour, minute, second
+        var temp_total_seconds = ++total_seconds;
+
+        save_time(temp_total_seconds);
+
+        setTotalTime();
+
+        if (--timer < 0 || STOP_TIMER == true) {
+            clearInterval(countdown);
+            $(".timer").empty().text("00:00");
+            console.log("Power Off with timer");
+            //enable power on
+            $(".on_btn").css('pointer-events', 'auto');
+            $(".on_btn i").css('color', 'blue');
+        }
+    }, 1000);
+}
+
+function setTotalTime() {
+    var usage_hours = Math.floor(total_seconds / 3600 );
+    var usage_minutes = Math.floor(total_seconds % 3600 / 60);
+    var usage_seconds = total_seconds % 60;
+
+    usage_hours = usage_hours < 10 ? "0" + usage_hours : usage_hours;
+    usage_minutes = usage_minutes < 10 ? "0" + usage_minutes : usage_minutes;
+    usage_seconds = usage_seconds < 10 ? "0" + usage_seconds : usage_seconds;
+
+    $(".run_time").empty().text(usage_hours + ":" + usage_minutes + ":" + usage_seconds);
+}
+
+function rotate(rotate_id) {
+    console.log("rotate");
+    if (!rotate_id) {
+        alert("Please enter direction");
+        return;
+    }
+
+    $move = 'left';
+
+    switch(rotate_id) {
+        case '1':
+            $move = 'left';
+            break;
+        case '2':
+            $move = 'right';
+            break;
+        case '3':
+            $move = 'up';
+            break;
+        case '4':
+            $move = 'down';
+    }
+
+    $.getJSON("/static/config.json", function(data) {
+        $camera_url = data.camera_url;
+        var destination_url = $camera_url + '/axis-cgi/com/ptz.cgi?move=' + $move;
+        
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                window.location.href = destination_url;
+            }
+        }
+        xhr.open('head', destination_url);
+        xhr.send(null);
+        
+    })
+
+}
+
+//store total seconds to data.json
+function save_time(temp_total_seconds) {
+    // $.getJSON($SCRIPT_ROOT + '/save/time', {
+    //     total_seconds : temp_total_seconds
+    // }, function(data) {
+    //     //console.log("Success");
+    // });
+    // localStorage.setItem("SECONDS", temp_total_seconds);
+}
+
+function menu_switch() {
+    $(".manual").on('click', function() {
+        auto_manual(false, true);
+        manual_show();
+    });
+
+    $(".auto").on('click', function() {
+        auto_manual(true, false);
+        auto_show();
+    });
+}
+
+function auto_manual(auto, manual) {
+    localStorage.setItem('AUTO', auto);
+    localStorage.setItem('MANUAL', manual);
+}
+
+function manual_show() {
+    //run time show
+    $(".run_time_lbl").show();
+    $(".run_time").show();
+
+    //timer show
+    $(".timer_select").hide();
+    $(".timer_lbl").hide();
+    $(".timer").hide();
+    $(".manual").addClass('active');
+    $(".auto").removeClass('active');
+}
+
+function auto_show() {
+    //run time hide
+    $(".run_time_lbl").hide();
+    $(".run_time").hide();
+    //timer show
+    $(".timer_select").show();
+    $(".timer_lbl").show();
+    $(".timer").show();
+    $(".auto").addClass('active');
+    $(".manual").removeClass('active');
+}
