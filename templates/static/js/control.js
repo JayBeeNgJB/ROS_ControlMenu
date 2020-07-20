5d97bc388b789b11f291b47f4082e6a849dbe27a
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2020-07-20 17:49:14 +0800

    add charging status, connected with robot status, emergency alert

--- templates/static/js/control.js
+++ templates/static/js/control.js
@@ -1,9 +1,3 @@
-window.addEventListener('load', (e) => {
-  current_page();
-
-  console.log("Loading control.. ");
-});
-
 // Connecting to ROS
 
 var ros = new ROSLIB.Ros({
@@ -11,16 +5,18 @@ var ros = new ROSLIB.Ros({
  });
 
  ros.on('connection', function() {
-     status();
-     console.log('Connected to websocket server.');
+    status(true);
+    console.log('Connected to websocket server.');
  });
 
  ros.on('error', function(error) {
-     console.log('Error connecting to websocket server: ', error);
+    status(false);
+    console.log('Error connecting to websocket server: ', error);
  });
  
  ros.on('close', function() {
-     console.log('Connection to websocket server closed.');
+    status(false);
+    console.log('Connection to websocket server closed.');
  });
 
  // Publishing a Topic
@@ -32,119 +28,206 @@ var ros = new ROSLIB.Ros({
  });
 
  var interval;
- var LINEAR = 2;
- var ANGULAR = 2;
+ var LINEAR = 0.05;
+ var ANGULAR = 0.05;
 
- $(document).ready(function() {
-     $(".btn_up").click(function () {
-         console.log("forward...")
-         clearInterval(interval);
-         interval = setInterval(UP, 1000);
-     });
+ window.addEventListener('load', (e) => {
+    current_page();
+    showSpeed(LINEAR);
+    battery_status();
+    emergency_alert();
+  
+    console.log("Loading control.. ");
+});
 
-     $(".btn_down").click(function () {
-         console.log ("backing.....");
-         clearInterval(interval);
-         interval = setInterval(DOWN, 1000);
-     });
+$(document).ready(function() {
+    $(".btn_up").click(function () {
+        console.log("forward...")
+        clearInterval(interval);
+        interval = setInterval(UP, 1000);
+    });
 
-     $(".btn_left").click(function () {
-         console.log ("Left.....");
-         clearInterval(interval);
-         interval = setInterval(LEFT, 1000);
-     });
+    $(".btn_down").click(function () {
+        console.log ("backing.....");
+        clearInterval(interval);
+        interval = setInterval(DOWN, 1000);
+    });
 
-     $(".btn_right").click(function () {
-         console.log ("Right.....");
-         clearInterval(interval);
-         interval = setInterval(RIGHT, 1000);
-     });
+    $(".btn_left").click(function () {
+        console.log ("Left.....");
+        clearInterval(interval);
+        interval = setInterval(LEFT, 1000);
+    });
 
-     $(".btn_stop").click(function () {
-         console.log ("STOP.....");
-         clearInterval(interval);
-         STOP();
-     });
+    $(".btn_right").click(function () {
+        console.log ("Right.....");
+        clearInterval(interval);
+        interval = setInterval(RIGHT, 1000);
+    });
 
-     //Linear speed and angular speed control
-     $(".btn_speed_up").click(function() {
-         LINEAR = LINEAR + 0.1;
-         console.log(LINEAR)
-     });
+    $(".btn_stop").click(function () {
+        console.log ("STOP.....");
+        clearInterval(interval);
+        STOP();
+    });
 
-     $(".btn_speed_down").click(function() {
-         if (LINEAR > 0.1) {
-             console.log("over 0");
-             LINEAR = LINEAR - 0.1;
-         }            
-         console.log(LINEAR);
-     });
+    //Linear speed and angular speed control
+    $("#speed_up").click(function() {
+        LINEAR = LINEAR + 0.05;
+        showSpeed(LINEAR);
+        console.log(LINEAR)
+    });
 
-     $(".test").click(function() {
-         var clearRoute = new ROSLIB.Service({
-             ros : ros,
-             name : '/clear',
-             serviceType : 'std_srvs/Empty'
-         });
+    $("#speed_down").click(function() {
+    console.log(LINEAR);
+    // LINEAR = LINEAR.toFixed(2);
+    if (LINEAR > 0.1) {
+        console.log("over 0");
+        LINEAR = LINEAR - 0.05;
+        showSpeed(LINEAR);
+    }            
+    console.log(LINEAR);
+    });
 
-         clearRoute.callService();
-     });
+    $(".test").click(function() {
+        var clearRoute = new ROSLIB.Service({
+            ros : ros,
+            name : '/clear',
+            serviceType : 'std_srvs/Empty'
+        });
 
-     $(".subcribe").click(function() {
-         console.log("Subcribe");
-         var subcribe_cmd = new ROSLIB.Topic({
-             ros : ros,
-             name : '/rosapi/get_loggers',
-             messageType : 'turtlesim/Pose'
-         });
+        clearRoute.callService();
+    });
 
-     });
+});
 
- });
+function UP() {
+    console.log("FORWarding");
+    publishData(LINEAR , 0, 0, 0, 0, 0);  
+}
 
- function UP() {
-   publishData(LINEAR , 0, 0, 0, 0, 0);  
- }
+function DOWN() {
+    publishData(0 - LINEAR, 0, 0, 0, 0, 0);  
+}
 
- function DOWN() {
-   publishData(0 - LINEAR, 0, 0, 0, 0, 0);  
- }
+function LEFT() {
+    publishData(0, 0, 0, 0, 0, LINEAR);  
+}
 
- function LEFT() {
-     publishData(0, 0, 0, 0, 0, ANGULAR);  
- }
+function RIGHT() {
+    publishData(0, 0, 0, 0, 0, 0 - LINEAR); 
+}
 
- function RIGHT() {
-   publishData(0, 0, 0, 0, 0, 0 - ANGULAR); 
- }
+function STOP() {
+    publishData(0, 0, 0, 0, 0, 0);
+}
 
- function STOP() {
-     publishData(0, 0, 0, 0, 0, 0);
- }
+function publishData(lx, ly, lz, ax, ay, az) {
+    var twist = new ROSLIB.Message({
+        linear : {
+            x : lx,
+            y : ly,
+            z : lz
+        },
+        angular : {
+            x : ax,
+            y : ay,
+            z : az
+        }
+    });
 
- function publishData(lx, ly, lz, ax, ay, az) {
-     var twist = new ROSLIB.Message({
-         linear : {
-             x : lx,
-             y : ly,
-             z : lz
-         },
-         angular : {
-             x : ax,
-             y : ay,
-             z : az
-         }
-     });
+    cmdVel.publish(twist);
+}
 
-     cmdVel.publish(twist);
- }
+function emergency_alert() {
+    var emer_switch = new ROSLIB.Topic({
+        ros : ros,
+        name : '/Wheel_Switch',
+        messageType : 'robot_msgs/Wheel_Switch'
+    });
 
- function status() {
-     $("#robot_status").text("Connected");
-     $("#robot_status").css("color", "green");
- }
+    emer_switch.subscribe(function(messages) {
+        if(messages.Switch) {
+            $(".emergency_alert").show();
+        } else {
+            $(".emergency_alert").hide();
+        }
+    }) 
+}
 
- function showSpeed(linear, angular) {
-     $("#linear_speed").text(linear);
-     $("#angular_speed").text(angular);
- }
+function emergency() {
+    console.log("Emergency");
+    // var emer_switch = new ROSLIB.Topic({
+    //     ros : ros,
+    //     name : '/Wheel_Switch',
+    //     messageType : 'robot_msgs/Wheel_Switch'
+    // });
+
+    // var emer_switch_val = new ROSLIB.Message({
+    //     Switch : 1
+    // });
+
+    // emer_switch.publish(emer_switch_val)
+}
+
+function dock() {
+    console.log("Docking");
+    clearInterval(interval);
+    STOP();
+    var auto_dock = new ROSLIB.Topic({
+        ros : ros,
+        name : '/Auto_Charging',
+        messageType : 'robot_msgs/Charging_Control'
+    });
+
+    var charge_val = new ROSLIB.Message({
+        auto_charging_flag : 1
+    });
+
+    auto_dock.publish(charge_val);
+}
+
+function status(status) {
+    if (status == true) {
+        $("#connection_status").html("<i class='fa fa-check'></i> Connected");
+        $("#connection_status").css({
+            "background-color" : "#22926c",
+            "color" : "#f5f7eb"
+            });
+    } else {
+        $("#connection_status").html("<i class='fa fa-exclamation-circle'></i> Not Connected");
+        $("#connection_status").css({
+            "background-color" : "#e62c2c",
+            "color" : "aquamarine"
+        });
+    }
+}
+
+function battery_status() {
+    console.log("Battery status");
+    var pms_status = new ROSLIB.Topic({
+        ros : ros,
+        name : '/PMS_get_status',
+        messageType : 'robot_msgs/PMS_get_status'
+    });
+
+    pms_status.subscribe(function(messages) {
+        if(messages.pms_charging_flag) {
+            $("#charging_status").show();
+            $("#battery_status").hide();
+            $("#battery_level_charge").text(messages.pms_battary_level + "% ");
+        } else {
+            $("#charging_status").hide();
+            $("#battery_status").show();
+            $("#battery_level").text(messages.pms_battary_level + "% ");
+        }  
+    })
+
+
+}
+
+function showSpeed(linear) {
+    linear = linear.toFixed(2);
+        $("#linear_speed").text(linear + "m/s");
+    //  $("#angular_speed").text(angular);
+}
